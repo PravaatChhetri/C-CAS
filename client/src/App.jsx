8ba4b7401ab0539a5a8cf7799e9c9cd71bfc3ce3
Nobdrel <nobudelma@gmail.com>
2023-10-02 22:58:38 +0600

    made the login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,54 +5,63 @@ import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
 import ContactUs from "./pages/ContactUs";
 import Menu from "./pages/Menu";
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 
 function Layout({ children }) {
   return (
     <>
-     <Navbar/>
-    {children}
+      <Navbar />
+      {children}
     </>
-   
   );
 }
 
 function App() {
   return (
     <div className="App">
-      
       <Router>
         <Routes>
-          <Route path="/" element={
-             <Layout>
-             <HomePage/>
-             </Layout>
-          } />
-          <Route exact path="/aboutus" element={
-            <Layout>
-            <AboutUs/>
-            </Layout>
-          }/>
-          <Route exact path="/contactus" element={
-            <Layout>
-            <ContactUs/>
-            </Layout>
-          }/>
-          <Route exact path="/login" element={
-            <Layout>
-            <Login/>
-            </Layout>
-          }/>
-          <Route exact path="/menu" element={
-            <Layout>
-            <Menu/>
-            </Layout>
-          }/>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <HomePage />
+              </Layout>
+            }
+          />
+          <Route
+            exact
+            path="/aboutus"
+            element={
+              <Layout>
+                <AboutUs />
+              </Layout>
+            }
+          />
+          <Route
+            exact
+            path="/contactus"
+            element={
+              <Layout>
+                <ContactUs />
+              </Layout>
+            }
+          />
+          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/menu"
+            element={
+              <Layout>
+                <Menu />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
-    
-    </div>  );
+    </div>
+  );
 }
 
 export default App;
